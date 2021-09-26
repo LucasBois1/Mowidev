bdbfe8c764ae13472672eac913a27132cd4b1d61
LucasBois1 <44686060+LucasBois1@users.noreply.github.com>
2021-09-25 21:12:25 -0300

    🚀 First Commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+// App.Use
+app.use(express.static('public'));
+
+// App.Get
+app.get('/', function (req, res) {
+    res.send(public/index.html)
+});
+
+app.listen(process.env.PORT || 8080)
+console.log("🚀 Server listening on port 8080");
